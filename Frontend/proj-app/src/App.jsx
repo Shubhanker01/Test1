55a39ce76c6133dd870bada6f2a8374def0bc586
Shubhanker01 <shubhanker40@gmail.com>
2024-05-31 22:27:16 +0530

    first commit

---
+++ Frontend/proj-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-red-300 text-3xl'>
+          Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
